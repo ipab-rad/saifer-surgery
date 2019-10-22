539a555e9db34ad77913e1a7492a81dce5782040
ultrasound <s1876262@ed.ac.uk>
2019-10-22 13:23:44 +0100

    Add package for planning with discrete points

---
+++ src/saif_planning/src/planning_discrete_pts/scripts/add_pts.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+
+import rospy
+import moveit_commander
+from moveit_commander import conversions
+from rospy.numpy_msg import numpy_msg
+import numpy as np 
+import os
+from sensor_msgs.msg import JointState
+
+
+
+class GraphBuilder(object):
+
+    def __init__(self, vertex_file, edge_file):
+
+        self.vertex_file = vertex_file
+        self.edge_file = edge_file
+
+        print(self.vertex_file)
+
+        if os.path.isfile(vertex_file):
+            nodes = list(np.load(vertex_file))
+        else:
+            print('no such file')
+            nodes = []
+            
+        if os.path.isfile(edge_file):
+            connections = list(np.load(edge_file))
+        else:
+            connections = [] 
+
+        self.nodes = nodes
+        self.connections = connections 
+        self.current_node = None 
+
+    def dist(self, node1, node2):
+        return np.linalg.norm(node1 - node2)
+
+    def get_graph_dist(self, node1, node2):
+        """number of transitions in shortest path between two nodes"""
+        path = self.find_shortest_path(node1, node2)
+        return len(path) - 1
+
+    def get_nodes_within_dist(self, node, dist):
+        return [n for n in range(self.nodes) if self.get_graph_dist(node, n) <= dist]
+
+    def find_shortest_path(self, node_index1, node_index2):
+
+        if node_index1 == node_index2:
+            return [node_index1]
+
+        path_found = False
+        parent_dict = {}
+        q = queue.Queue()
+        q.put(node_index1)
+        visited = {node_index1}
+
+        while not path_found:
+            current = q.get()
+            edges = [e.remove(current).pop() for e in self.connections if current in e]
+            children = [c for c in edges if c not in visited]
+
+            for c in children:
+                parent_dict[c] = current
+                q.put(c)
+                visited.add(c)
+
+            if node_index2 is in children:
+                path_found = True 
+
+        point = node_index2
+        waypoints = [point]
+
+        while point != node_index1:
+            point = parent_dict[point]
+            waypoints.insert(0, point)
+
+        return waypoints
+
+    def findClosestNode(self, state):
+        dists = [self.dist(state, n) for n in self.nodes]
+        return dists.index(min(dists)), min(dists)
+
+
+    def state2index(self, state):
+        return self.nodes.index(state)
+
+    def index2state(self, index):
+        return self.nodes[index]
+
+    def store_node(self, data):
+
+        position = np.array(data.position)
+        thresh = .1
+        add_thresh = .5
+        dist_list = [self.dist(node, position) for node in self.nodes]
+
+        if len(self.nodes) == 0:
+            self.nodes.append(position)
+            data_index = len(self.nodes) - 1
+
+        else:
+            min_dist = min(dist_list)
+            if min_dist < thresh:
+                data_index = dist_list.index(min_dist) 
+            elif min_dist > add_thresh:
+                self.nodes.append(position)
+                data_index = len(self.nodes) - 1
+
+            else:
+                data_index = self.current_node
+        if len(self.nodes) > 1:
+            self.add_connection(self.current_node, data_index)
+        self.current_node = data_index 
+
+
+    def add_connection(self, index1, index2):
+        if {index1, index2} not in self.connections and index1 != index2:  
+            self.connections.append({index1, index2})
+        else:
+            return 
+
+
+    def build_graph(self):
+
+        rospy.init_node('set_pts', anonymous=True)
+
+        rospy.Subscriber("/joint_states", numpy_msg(JointState), self.store_node)
+
+        rospy.spin()
+
+        np.save(self.vertex_file, np.array(self.nodes))
+        np.save(self.edge_file, np.array(self.connections))
+
+if __name__ == "__main__":
+
+    
+    gb = GraphBuilder('test_graph_pts.npy', 'test_graph_edges.npy')
+    gb.build_graph()
+
+
+    
