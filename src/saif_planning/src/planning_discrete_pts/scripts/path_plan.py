37ec5dbca96c1d2b9315be1bdad6205c1ea84972
ultrasound <s1876262@ed.ac.uk>
2019-10-23 18:16:09 +0100

    add file with planning functions

--- src/saif_planning/src/planning_discrete_pts/scripts/path_plan.py
+++ src/saif_planning/src/planning_discrete_pts/scripts/path_plan.py
@@ -1,5 +1,7 @@
-from add_pts import GraphBuilder
-
+#!/usr/bin/env python
+from add_pts import PlanningGraph
+import rospy
+import moveit_commander
 
 
 def planJointWaypoints(start, stop, graph, max_dist = .5):
@@ -8,14 +10,34 @@ def planJointWaypoints(start, stop, graph, max_dist = .5):
     stop_node, d2 = graph.findClosestNode(stop)
 
     if d1 > .5 or d2 > .5:
-        print("no safe solution guarunteed")
+        print("no safe solution guaranteed")
         return
 
-    waypoints = graph.find_shortest_path(start_node, stop_node)
+    waypoints = graph.findShortestPath(start_node, stop_node)
 
     return waypoints
 
     
+def planAndExecuteFromWaypoints(start, stop, graph, move_group, max_dist = .5):
+
+    group = moveit_commander.MoveGroupCommander(move_group)
+
+    start_node, d1 = graph.findClosestNode(start)
+    stop_node, d2 = graph.findClosestNode(stop)
+
+    if d1 > .5 or d2 > .5:
+        print("no safe solution guaranteed")
+        return
+
+    waypoints = graph.findShortestPath(start_node, stop_node)
+
+    (plan, fraction) = group.compute_cartesian_path(
+                                   waypoints,   
+                                   0.01,        
+                                   0.0)
+
+    group.execute(plan, wait=True)
+
 
 if __name__ == "__main__":
 
@@ -23,7 +45,7 @@ if __name__ == "__main__":
 
     rospy.init_node('set_pts', anonymous=True)
 
-    gb = GraphBuilder('test_graph_pts.npy', 'test_graph_edges.npy')
+    gb = PlanningGraph('test_graph_pts.npy', 'test_graph_edges.npy')
 
     group = moveit_commander.MoveGroupCommander(group_name)
 
