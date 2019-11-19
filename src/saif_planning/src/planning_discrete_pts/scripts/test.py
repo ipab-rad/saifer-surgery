547999e23edf81cef380f3950ce408920b4ab5d3
ultrasound <s1876262@ed.ac.uk>
2019-11-19 17:56:01 +0000

    update test.py

--- src/saif_planning/src/planning_discrete_pts/scripts/test.py
+++ src/saif_planning/src/planning_discrete_pts/scripts/test.py
@@ -9,12 +9,12 @@ import numpy as np
 
 if __name__ == "__main__":
 
-    target_im = cv2.imread('left0000.jpg')
+    target_im = cv2.imread('pink_ball.jpg')
     target_im = np.expand_dims(target_im, axis=0)
 
     img_shape=(480,640,3)
 
-    model = InceptionV3(include_top=False, weights='imagenet', input_tensor=None, input_shape=img_shape, pooling='avg', classes=1000)
+    model = InceptionV3(include_top=True, weights='imagenet', input_tensor=None, input_shape=img_shape, pooling='avg', classes=1000)
 
     res = np.array(model.predict(target_im)).flatten()
-    print(np.shape(res))
+    print(res)
