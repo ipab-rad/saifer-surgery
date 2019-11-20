ce1065701a4f2d36953214e67bd46a39777aabb7
UR10_console <michael.burke@ed.ac.uk>
2019-11-20 12:39:42 +0000

    Stiff (due to poor ft ) calibration) compliance node, need to send zero velocity to arm to stop springy bounce.

--- src/saif_control/ft_compensate/scripts/ft_compensate.py
+++ src/saif_control/ft_compensate/scripts/ft_compensate.py
@@ -14,7 +14,6 @@ class FT_COMP():
     def __init__(self):
         rospy.init_node('ft_compensator', anonymous=True)
 
-        rospy.Subscriber("/red/robotiq_ft_wrench", WrenchStamped, self.callback,queue_size=1)
         self.pub = rospy.Publisher("/red/robotiq_ft_wrench_compensated",WrenchStamped,queue_size=1)
 
         self.robot = moveit_commander.RobotCommander()
@@ -27,30 +26,39 @@ class FT_COMP():
         T = np.genfromtxt(data_path+'T_'+group_name+'.txt')
         theta = np.genfromtxt(data_path+'theta_'+group_name+'.txt')
 
-
-#        self.Freg = KNeighborsRegressor(n_neighbors=1).fit(theta,F)
-	self.Freg = DecisionTreeRegressor(max_depth=20).fit(theta,F)
-        self.Treg = KNeighborsRegressor(n_neighbors=1).fit(theta,T)#DecisionTreeRegressor(max_depth=20).fit(theta,T)
+        self.Freg_x = KNeighborsRegressor(n_neighbors=15).fit(theta,F[:,0])
+        self.Freg_y = KNeighborsRegressor(n_neighbors=15).fit(theta,F[:,1])
+        self.Freg_y = KNeighborsRegressor(n_neighbors=15).fit(theta,F[:,2])
+#	self.Freg = DecisionTreeRegressor(max_depth=20).fit(theta,F)
+        self.Treg_x = KNeighborsRegressor(n_neighbors=15).fit(theta,T[:,0])#DecisionTreeRegressor(max_depth=20).fit(theta,T)
+        self.Treg_y = KNeighborsRegressor(n_neighbors=15).fit(theta,T[:,1])#DecisionTreeRegressor(max_depth=20).fit(theta,T)
+        self.Treg_z = KNeighborsRegressor(n_neighbors=15).fit(theta,T[:,2])#DecisionTreeRegressor(max_depth=20).fit(theta,T)
 
 
     def run(self):
+        rospy.Subscriber("/red/robotiq_ft_wrench", WrenchStamped, self.callback,queue_size=1)	
 	rospy.spin()
 
     def callback(self,msg):
 
         pose = self.group.get_current_pose().pose
        	theta = R.from_quat(np.array((pose.orientation.x,pose.orientation.y,pose.orientation.z,pose.orientation.w)))
-        self.F = self.Freg.predict(theta.as_euler('zyx').reshape(1,-1))
-        self.T = self.Treg.predict(theta.as_euler('zyx').reshape(1,-1))
+	theta = theta.as_euler('zxy').reshape(1.-1)
+        self.Fx = self.Freg_x.predict(theta)
+        self.Fy = self.Freg_y.predict(theta)
+        self.Fz = self.Freg_z.predict(theta)
+        self.Tx = self.Treg_x.predict(theta)
+        self.Ty = self.Treg_y.predict(theta)
+        self.Tz = self.Treg_z.predict(theta)
 
 	msg_new = msg	
-        msg_new.wrench.force.x = msg.wrench.force.x - self.F[0,0]
-       	msg_new.wrench.force.y = msg.wrench.force.y - self.F[0,1]
-        msg_new.wrench.force.z = msg.wrench.force.z - self.F[0,2]
+        msg_new.wrench.force.x = msg.wrench.force.x - self.Fx
+       	msg_new.wrench.force.y = msg.wrench.force.y - self.Fy
+        msg_new.wrench.force.z = msg.wrench.force.z - self.Fz
 
-       	msg_new.wrench.torque.x = msg.wrench.torque.x - self.T[0,0]
-        msg_new.wrench.torque.y = msg.wrench.torque.y - self.T[0,1]
-       	msg_new.wrench.torque.z = msg.wrench.torque.z - self.T[0,2]
+       	msg_new.wrench.torque.x = msg.wrench.torque.x - self.Tx
+        msg_new.wrench.torque.y = msg.wrench.torque.y - self.Ty
+       	msg_new.wrench.torque.z = msg.wrench.torque.z - self.Tz
 
 	f_diff = np.sqrt(msg_new.wrench.force.x**2 + msg_new.wrench.force.y**2 + msg_new.wrench.force.z**2)
 	print(f_diff)
--- src/saif_control/ft_compliance/src/inverse_dynamics.cpp
+++ src/saif_control/ft_compliance/src/inverse_dynamics.cpp
@@ -8,7 +8,7 @@
 #include <moveit/robot_model/robot_model.h>
 #include <moveit/robot_state/robot_state.h>
 
-#define FTHRESH 600.0
+#define FTHRESH 80.0
 
 class IDyn {
 	public:
@@ -42,12 +42,13 @@ class IDyn {
 
 IDyn::IDyn(void)
 {
-	sub1 = n.subscribe("/red/robotiq_ft_wrench",1,&IDyn::ft_callback,this);	
+	//sub1 = n.subscribe("/red/robotiq_ft_wrench",1,&IDyn::ft_callback,this);		
+	sub1 = n.subscribe("/red/robotiq_ft_wrench_compensated",1,&IDyn::ft_callback,this);	
 	sub2 = n.subscribe("/joint_states",1,&IDyn::jt_callback,this);	
 	
 	pub = n.advertise<trajectory_msgs::JointTrajectory>("/red/vel_test",1);
 	group_name = "red_arm";
-	first = true;
+//	first = true;
 	Fbase = Eigen::MatrixXd(6,1);
 	jac = false;
 	ft_meas = false;
@@ -64,67 +65,68 @@ IDyn::~IDyn(void)
 
 void IDyn::ft_callback(const geometry_msgs::WrenchStamped& msg)
 {
-	if (first)
-	{
-		Fbase << -msg.wrench.force.x, -msg.wrench.force.y, msg.wrench.force.z, msg.wrench.torque.x,msg.wrench.torque.y,msg.wrench.torque.z;
-		first = false;
-	}
+//	if (first)
+//	{
+//		Fbase << -msg.wrench.force.x, -msg.wrench.force.y, msg.wrench.force.z, msg.wrench.torque.x,msg.wrench.torque.y,msg.wrench.torque.z;
+//		first = false;
+//	}
 
 	if (jac)
 	{
 
 		Eigen::MatrixXd ft(6,1);
-		ft << -msg.wrench.force.x, -msg.wrench.force.y, msg.wrench.force.z, msg.wrench.torque.x,msg.wrench.torque.y,msg.wrench.torque.z;
+		ft << msg.wrench.force.y, -msg.wrench.force.x, msg.wrench.force.z, msg.wrench.torque.y,-msg.wrench.torque.x,msg.wrench.torque.z;
 		ROS_INFO("FT: %2.2f. Threshold: %2.2f.",ft.squaredNorm(),FTHRESH);
-		if ((Fbase-ft).squaredNorm() > 0)
+		//if ((Fbase-ft).squaredNorm() > 0)
+		if (ft.squaredNorm() < FTHRESH)
 		{
-
-			Eigen::MatrixXd K = Eigen::MatrixXd::Zero(6,6);
-			for (int i = 0; i < 6; i++)
-			{
-				if (i < 3)
-				{
-					K(i,i) = 0.01;
-				}
-				else
-				{
-					K(i,i) = 0.0;
-				}
-			}
-	
-
-			Eigen::MatrixXd twist = K*(Fbase-ft);
-			Eigen::MatrixXd joint_vel = Jinv*twist; 
-
-			trajectory_msgs::JointTrajectory jt;
-			jt.header = msg.header;
-			jt.joint_names =  joint_names;
-			trajectory_msgs::JointTrajectoryPoint point;
-			point.time_from_start = ros::Duration(1.0);
-			point.positions = joint_angles;
-	
-			for (int i = 0; i < int(joint_vel.rows()); i++)
-			{
-				if (joint_vel(i,0) > 0.5)
-                                {
-                                        joint_vel(i,0) = 0.5;
-                                }
-                                if (joint_vel(i,0) < -0.5)
-                                {
-                                        joint_vel(i,0) = -0.5;
-                                }
-				point.positions[i] = point.positions[i] + 0.1*joint_vel(i,0);
-				point.velocities.push_back(joint_vel(i,0));
-			}
-			jt.points.push_back(point);	
-			for (int i = 0; i < int(joint_vel.rows()); i++)
-			{
-				point.velocities[i] = 0.0;
-			}
-			point.time_from_start = ros::Duration(2.0);	
-			jt.points.push_back(point);	
-			pub.publish(jt);	
+			ft = ft*0.0;
 		}
+		
+
+		Eigen::MatrixXd K = Eigen::MatrixXd::Zero(6,6);
+		for (int i = 0; i < 6; i++)
+		{
+			if (i < 3)
+			{
+				K(i,i) = 0.01;
+			}
+			else
+			{
+				K(i,i) = 0.05;
+			}
+		}
+	
+//		Eigen::MatrixXd twist = K*(Fbase-ft);
+		Eigen::MatrixXd twist = -K*ft;
+		Eigen::MatrixXd joint_vel = Jinv*twist; 
+		trajectory_msgs::JointTrajectory jt;
+		jt.header = msg.header;
+		jt.joint_names =  joint_names;
+		trajectory_msgs::JointTrajectoryPoint point;
+		point.time_from_start = ros::Duration(1.0);
+		point.positions = joint_angles;
+		for (int i = 0; i < int(joint_vel.rows()); i++)
+		{
+			if (joint_vel(i,0) > 0.5)
+                        {
+        	                joint_vel(i,0) = 0.5;
+                        }
+                        if (joint_vel(i,0) < -0.5)
+                        {
+                	        joint_vel(i,0) = -0.5;
+                        }
+			point.positions[i] = point.positions[i] + 0.1*joint_vel(i,0);
+			point.velocities.push_back(joint_vel(i,0));
+		}
+		jt.points.push_back(point);	
+		for (int i = 0; i < int(joint_vel.rows()); i++)
+		{
+			point.velocities[i] = 0.0;
+		}
+		point.time_from_start = ros::Duration(2.0);	
+		jt.points.push_back(point);	
+		pub.publish(jt);			
 	}
 }
 
