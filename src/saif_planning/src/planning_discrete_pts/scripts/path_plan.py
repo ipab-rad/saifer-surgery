32cd612a42cf4643b3457ffe3115febcf9c248dc
UR10_console <michael.burke@ed.ac.uk>
2019-12-06 16:37:17 +0000

    fix more merge conflict

--- src/saif_planning/src/planning_discrete_pts/scripts/path_plan.py
+++ src/saif_planning/src/planning_discrete_pts/scripts/path_plan.py
@@ -33,7 +33,7 @@ def planAndExecuteFromWaypoints(start, stop, graph, move_group, max_dist = .5):
     #print("waypoints: " + str(waypoints))
 
     for w in waypoints:
-	#print("moving to waypoint: " + str(w))
+	print("moving to waypoint: " + str(w))
         w = graph.index2state(w)
         
         group.go(w, wait=True)
