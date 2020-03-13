3c28ab5c891c7c4bf16d053cef4fa73f4dc7d7f9
Michael Burke <michaelburke@ieee.org>
2020-03-13 10:06:24 +0000

    Moved visual irl to submodule

--- src/saif_learning/visual_irl/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-setup_args = generate_distutils_setup(
-    packages=['visual_irl'],
-	package_dir={'': 'scripts'}
-)
-
-setup(**setup_args)
